48e5bee353916f91f1e3bb0b8880b627da82c28b
FeRD (Frank Dana) <ferdnyc@gmail.com>
2018-08-19 11:41:19 -0400

    Updated to check for bad f29 binutils

--- find_suspects.py
+++ find_suspects.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python2
+#
+# Script to drive rpmfusion rebuild of packages corrupted by bad binutils
+# versions. Based on Fedora releng find-bad-builds.py and:
 #
 # find-failures.py - A utility to discover failed builds in a given tag
 #                    Output is currently rough html
@@ -6,13 +9,15 @@
 # Copyright (C) 2013 Red Hat Inc,
 # SPDX-License-Identifier:	GPL-2.0+
 #
-#
 # Authors:
 #     Jesse Keating <jkeating@redhat.com>
 #     Ralph Bean <rbean@redhat.com>
 #
 
 from __future__ import print_function
+
+import sys
+
 import koji
 import operator
 import datetime
@@ -24,12 +29,18 @@ from requests.packages.urllib3.util.retry import Retry
 
 # Set some variables
 # Some of these could arguably be passed in as args.
-buildtag = 'f29-rebuild' # tag to check
-desttag = 'f29' # Tag where fixed builds go
-epoch = '2018-07-12 17:00:00.000000' # Date to check for failures from
-failures = {} # dict of owners to lists of packages that failed.
-failed = [] # raw list of failed packages
+tagglob = 'f29*' # glob matching tags to check
 
+# Date range within which bad builds may be found
+epoch_start = '2018-07-26 13:00:00.000000'
+epoch_end = '2018-07-31 00:00:00.000000'
+
+# the binutils versions that caused bad builds
+bad_binutils_ver = '2.31.1'
+bad_binutils_rels = ['4.fc29', '5.fc29', '6.fc29', '7.fc29']
+
+rebuilddict = {} # dict of owners to lists of packages needing rebuild.
+rebuilds = [] # raw list of packages needing rebuild.
 
 def retry_session():
     session = requests.Session()
@@ -46,99 +57,74 @@ def retry_session():
     return session
 
 
-def get_failed_builds(kojisession, epoch, buildtag, desttag):
-    """This function returns list of all failed builds since epoch within
-    buildtag that were not rebuilt succesfully in desttag
+def get_builds(kojisession, buildtag):
+    """This function returns list of all builds since epoch within
+    buildtag that match our criteria for suspect builds
 
     Keyword arguments:
     kojisession -- connected koji.ClientSession instance
-    epoch -- string representing date to start looking for failed builds
-             from. Format: "%F %T.%N"
-    buildtag -- tag where to look for failed builds (usually fXX-rebuild)
-    desttag -- tag where to look for succesfully built packages
+    buildtag -- tag where to look for builds
     """
-    # Get a list of failed build tasks since our epoch
-    failtasks = sorted(kojisession.listBuilds(createdAfter=epoch, state=3),
-                       key=operator.itemgetter('task_id'))
 
-    # Get a list of successful builds tagged
-    goodbuilds = kojisession.listTagged(buildtag, latest=True)
+    # Get a list of builds in our build tag
+    destbuilds = kojisession.listTagged(buildtag, latest=True, inherit=False)
 
-    # Get a list of successful builds after the epoch in our dest tag
-    destbuilds = kojisession.listTagged(desttag, latest=True, inherit=True)
+    suspects = [] # raw list of potential rebuilds.
+
     for build in destbuilds:
-        if build['creation_time'] > epoch:
-            goodbuilds.append(build)
+        if (build['creation_time'] > epoch_start) and (build['creation_time'] < epoch_end):
+            suspects.append(build)
+    print("Checking {} builds in tag {}".format(len(suspects),buildtag))
 
-    pkgs = kojisession.listPackages(desttag, inherited=True)
+    needbuild = []
 
-    # get the list of packages that are blocked
-    pkgs = sorted([pkg for pkg in pkgs if pkg['blocked']],
-                  key=operator.itemgetter('package_id'))
-
-    # Check if newer build exists for package
-    failbuilds = []
-    for build in failtasks:
-        if ((not build['package_id'] in [goodbuild['package_id'] for goodbuild in goodbuilds]) and (not build['package_id'] in [pkg['package_id'] for pkg in pkgs])):
-            failbuilds.append(build)
-
-    # Generate taskinfo for each failed build
-    kojisession.multicall = True
-    for build in failbuilds:
-        kojisession.getTaskInfo(build['task_id'], request=True)
-
-    taskinfos = kojisession.multiCall()
-    for build, [taskinfo] in zip(failbuilds, taskinfos):
-        build['taskinfo'] = taskinfo
-    # Get owners of the packages with failures
-    http = retry_session()
-    for build in failbuilds:
-        build['package_owner'] = get_package_owner(http, build['package_name'])
-    return failbuilds
-
-def get_package_owner(http, package):
-    url = 'https://src.fedoraproject.org/api/0/rpms/{0}'.format(package)
-    response = http.get(url, timeout=30)
-    if not bool(response):
-        return 'releng'
-    return response.json()['access_users']['owner'][0]
+    for build in suspects:
+        sys.stdout.write('.')
+        for task in kojisession.getTaskChildren(build['task_id']):
+            if build in needbuild:
+                continue
+            if task['method'] == 'buildArch':
+                for rootid in kojisession.listBuildroots(taskID=task['id']):
+                    for pkg in kojisession.listRPMs(componentBuildrootID=rootid['id']):
+                        if (pkg['name'] == 'binutils') and (pkg['version'] == bad_binutils_ver):
+                            if pkg['release'] in bad_binutils_rels:
+                                #print("task {}: found bad binutils version {}-{}".format(
+                                #       task['id'],pkg['version'],pkg['release']), file=sys.stderr,)
+                                sys.stdout.write('\b!')
+                                needbuild.append(build)
+    return needbuild
 
 
 if __name__ == '__main__':
     # Create a koji session
-    kojisession = koji.ClientSession('https://koji.fedoraproject.org/kojihub')
+    kojisession = koji.ClientSession('http://koji.rpmfusion.org/kojihub')
 
-    failbuilds = get_failed_builds(kojisession, epoch, buildtag, desttag)
+    taglist = kojisession.search(terms=tagglob, type="tag", matchType="glob")
 
+    builds = []
+    for tag in taglist:
+        builds.extend(get_builds(kojisession, tag['name']))
 
     # Generate the dict with the failures and urls
-    for build in failbuilds:
-        if not build['taskinfo']['request'][1] == buildtag:
-            continue
-        taskurl = 'https://koji.fedoraproject.org/koji/taskinfo?taskID=%s' % build['task_id']
-        owner = build['package_owner']
-        pkg = build['package_name']
-        if not pkg in failed:
-            failed.append(pkg)
-        failures.setdefault(owner, {})[pkg] = taskurl
+    for build in builds:
+        taskurl = 'https://koji.rpmfusion.org/koji/taskinfo?taskID=%s' % build['task_id']
+        owner = build['owner_name']
+        pkg = build['name']
+        if not pkg in rebuilds:
+            rebuilds.append(pkg)
+        rebuilddict.setdefault(owner, {})[pkg] = taskurl
 
     now = datetime.datetime.now()
     now_str = "%s UTC" % str(now.utcnow())
-    print('<html><head>')
-    print('<title>Packages that failed to build as of %s</title>' % now_str)
-    print('<style type="text/css"> dt { margin-top: 1em } </style>')
-    print('</head><body>')
-    print("<p>Last run: %s</p>" % now_str)
-
-    print('%s failed builds:<p>' % len(failed))
+    print('\nPackages that need rebuilding as of {} ({} total)'.format(now_str,len(rebuilds)))
 
     # Print the results
-    print('<dl>')
-    print('<style type="text/css"> dt { margin-top: 1em } </style>')
-    for owner in sorted(failures.keys()):
-        print('<dt>%s (%s):</dt>' % (owner, len(failures[owner])))
-        for pkg in sorted(failures[owner].keys()):
-            print('<dd><a href="%s">%s</a></dd>' % (failures[owner][pkg], pkg))
-    print('</dl>')
-    print('</body>')
-    print('</html>')
+    for owner in sorted(rebuilddict.keys()):
+        print()
+        print("{} ({}):".format(owner, len(rebuilddict[owner])))
+        for pkg in sorted(rebuilddict[owner].keys()):
+            if(len(pkg) > 15):
+                print("  {}\n                  {}".format(pkg,rebuilddict[owner][pkg]))
+            else:
+                print("  {0:<15} {1}".format(pkg,rebuilddict[owner][pkg]))
+
